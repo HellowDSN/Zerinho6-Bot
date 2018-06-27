5ccb82937f4db32bd12237278ba4120f22ec96e6
zerinho6 <zerinho6@gmail.com>
2018-06-26 21:42:58 -0300

    Update clear.js

--- comandos/clear.js
+++ comandos/clear.js
@@ -1,7 +1,7 @@
 var config = require( "../config.json" );
 modele.exports = {
 	run: ( bot , message , args ) => {
-		if( !message.member.hasPermission( "MANAGE_MESSAGES" ) ) return message.reply( "Você precisa da permissão de gerenciar mensagens para executar esse comando." );
+	if( !message.member.hasPermission( "MANAGE_MESSAGES" ) ) return message.reply( "Você precisa da permissão de gerenciar mensagens para executar esse comando." );
         if( !message.channel.permissionsFor( bot.user.id ).has( "MANAGE_MESSAGES" ) ) return message.reply( "Eu preciso da permissão de gerenciar mensagens para executar esse comando." );
 	
 	    if ( args[ 0 ] ) {
@@ -9,18 +9,18 @@ modele.exports = {
 				var toDelete = parseInt( args[ 0 ] );
 				if ( toDelete.length < 3 ) {
 					try {
-						message.channel.bulkDelete( toDelete + 1 , true );
+					    message.channel.bulkDelete( toDelete + 1 , true );
 					    toDelete === 1 ? message.channel.send( toDelete + " mensagem foi deletada." ) : message.channel.send( toDelete + " mensagens foram deletadas." ); 
-				    } catch ( e ) {
+				        } catch ( e ) {
 						var k = require( "../comandos/avatar.js" );
 						k.special( message , message.member , e );
-				    }
-			    } else {
+				        }
+				} else {
 				    message.reply( "A quantidade no qual eu devo deletar deve ser menos de 100." );
-			    }
-	        } else {
-			    message.reply( "A quantidade de mensagens que eu devo deletar deve ser em numeral." );
-		    }
+				}
+			} else {
+				message.reply( "A quantidade de mensagens que eu devo deletar deve ser em numeral." );
+			}
 	    } else {
 		    message.reply( "Apagar quantas mensagens? Se não sabe usar o comando, use o comando de ajuda." );
 	    }  

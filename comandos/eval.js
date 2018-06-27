58db577437633e938bdd6297704c5cdba8644515
zerinho6 <zerinho6@gmail.com>
2018-06-26 21:45:00 -0300

    Update eval.js

--- comandos/eval.js
+++ comandos/eval.js
@@ -1,22 +1,22 @@
 var config = require( "../config.json" );
 module.exports = {
 	run: ( bot , message , args ) => {
-		if( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
-	    if( message.author.id !== config.dono ) return;
+	    if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
+	    if ( message.author.id !== config.dono ) return;
 	
 	    function srun( code ) {
-			var Discord = require( "discord.js" ),
+		    var Discord = require( "discord.js" ),
 		    embed = new Discord.RichEmbed();
 			
 			try {
-				eval( code );
-			    embed.setColor( message.member.displayHexColor );
+			eval( code );
+			embed.setColor( message.member.displayHexColor );
 		        embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
 		        embed.addField( "Codigo", "```JavaScript\n" + code + "```" );
 		        embed.addField( "typeof", "```JavaScript\n" + typeof( code ) + "```" );
 		        message.channel.send( embed );
 	        } catch ( e ) {
-				embed.setColor( message.member.displayHexColor );
+		    embed.setColor( message.member.displayHexColor );
 	    	    embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
 	    	    embed.addField( "Erro", "```JavaScript" + e + "```" );
 	    	    message.channel.send( embed );

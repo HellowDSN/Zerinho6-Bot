b3f5d697e6b3ac461bef99442d89653f79086c86
zerinho6 <zerinho6@gmail.com>
2018-06-26 21:56:18 -0300

    Update reload.js

--- comandos/reload.js
+++ comandos/reload.js
@@ -4,18 +4,18 @@ module.exports = {
 		if( message.author.id === config.dono ) {
 			if( args[ 0 ] < 1 ) {
 				message.channel.send( "Ops, cade o nome do comando seu lezado?" ); 
-		    } else {
+			} else {
 				try {
 					delete require.cache[ require.resolve( `./${args[0]}.js` ) ];
-    	            message.channel.send( "Comando resetado." ); 
+    	                                message.channel.send( "Comando resetado." ); 
 				} catch ( e ) {
 					var k = require( "../comandos/avatar.js" );
 					k.special( message , message.member , e );
 				}
-            }
-	    } else {
+			}
+		} else {
 			message.channel.send( "Este comando é privado, para saber quando um comando é privado ou precisa de uma permissão adicional de cargo, execute **" + config.prefixes[ 0 ] + "comando**" );
-        }
+		}
 	},
 	description: "Recarrega um comando.",
 	photo: "https://i.imgur.com/gK6S7VH.png",

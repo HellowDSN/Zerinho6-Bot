6facf1a314557a0c52c1db09c32f1c28dae46fb1
zerinho6 <zerinho6@gmail.com>
2018-06-26 21:52:44 -0300

    Update ping.js

--- comandos/ping.js
+++ comandos/ping.js
@@ -3,17 +3,17 @@ module.exports = {
     run: ( bot , message , args ) => {
 		if( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
 		
-		var old = new Date(),
+	    var old = new Date(),
 	    Discord = require( "discord.js" ),
-		k = require( "../comandos/avatar.js" ),
-		user = message.author,
+	    k = require( "../comandos/avatar.js" ),
+	    user = message.author,
 	    embed = new Discord.RichEmbed();
 	    embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
 	    message.channel.send( embed ).then( message => {
-			var now = new Date();
+	            var now = new Date();
 		    function emoji( ping ) {
-				var array = [ "<:circulo_vermelho:428733167877226497>" , "<:circulo_verde:428732952092999680>" , "<:circulo_laranja:428733119399460865>" , "<:circulo_amarelo:428733064605335573>" ]; 
-				if ( ping > 3000 ) {
+			    var array = [ "<:circulo_vermelho:428733167877226497>" , "<:circulo_verde:428732952092999680>" , "<:circulo_laranja:428733119399460865>" , "<:circulo_amarelo:428733064605335573>" ]; 
+			    if ( ping > 3000 ) {
 					return array[ 0 ];
 			    } else if ( ping > 1500 ) {
 				    return array[ 2 ];
@@ -24,17 +24,16 @@ module.exports = {
 			    }
 		    };
 			
-			embed.setTimestamp();
+		    embed.setTimestamp();
 		    embed.setColor( message.member.displayHexColor );
 		    embed.setAuthor( user.username, user.displayAvatarURL );
 		    embed.addField( emoji( Math.round( now - old ) ) + " | Pong", "```JavaScript\n" + Math.round( now - old ) + "ms```" );
 		    embed.addField( ":gear: **| RAM**" , "```JavaScript\n" + `${ Math.round( process.memoryUsage().heapUsed / 1024 / 1024 ) }MB\u200B` + "```" );
-		    
-			try {
-				message.edit( embed );
-			} catch ( e ) {
-				k.special( message , message.member , e );
-			}
+		    try {
+			    message.edit( embed );
+		    } catch ( e ) {
+			    k.special( message , message.member , e );
+		    }
 	    });
 	},
 	description: "Mostra o ping do bot.",

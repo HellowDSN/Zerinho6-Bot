931ea2e5125ab74bdad164589ccc5694a012ccaa
zerinho6 <zerinho6@gmail.com>
2018-06-26 22:03:15 -0300

    Update ready.js

--- eventos/ready.js
+++ eventos/ready.js
@@ -1,5 +1,5 @@
 exports.run = function() {
 	var chalk = require( "chalk" ),
 	config = require( "../config.json" );
-    console.log( chalk.green( "Oi " ) + chalk.blue( config.name ) );
+        console.log( chalk.green( "Oi " ) + chalk.blue( config.name ) );
 };

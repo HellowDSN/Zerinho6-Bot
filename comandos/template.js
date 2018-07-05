d4087c74003193d671f100b474e38ea16d46a99c
zerinho6 <zerinho6@gmail.com>
2018-07-05 17:33:27 -0300

    Update template.js

--- comandos/template.js
+++ comandos/template.js
@@ -1,27 +1,29 @@
 var config = require( "../config.json" );
 module.exports = {
-	run: ( bot, message , args ) => {
-		if( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
-			try {
-				var fs = require( "fs" );
-				fs.readFile( `./template.js`, "utf8" , ( err , data ) => {
-				    var Discord = require( "discord.js" ),
-				    user = message.author,
-				    embed = new Discord.RichEmbed();
-				    embed.setAuthor( user.username, user.displayAvatarURL );
-				    embed.addField( "**Template**" , "```JavaScript\n" + data + "```" );
-				    embed.setColor( message.member.displayHexColor );
-				    embed.setTimestamp();
-				    embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
-				    message.channel.send( embed );
-				});
-			} catch ( e ) {
-				var k = require( "../comandos/avatar.js" );
-				k.special( message , message.member , e );
-			}
+	run: ( bot , message , args ) => {
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
+		
+		var fs = require( "fs" ),
+		helper = require( "../helper.js" ),
+		user = message.author,
+		Discord = require( "discord.js" ),
+		embed = new Discord.RichEmbed();
+		
+		try {
+			fs.readFile( `./template.js`, "utf8" , ( err , data ) => {
+				embed.setAuthor( user.username, user.displayAvatarURL );
+				embed.addField( "**Template**" , helper.embed( data , "JavaScript" ) );
+				embed.setColor( message.member.displayHexColor );
+				embed.setTimestamp();
+				embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
+				message.channel.send( embed );
+			});
+		} catch ( e ) {
+			helper.error_message( message , message.member , e );
+		}
 	},
 	description: "Envia uma mensagem em embed",
 	photo: "https://i.imgur.com/QGRnvlp.png",
 	permission: "Nenhuma permissão necessaria.",
 	use: `${ config.prefixes[ 0 ] }embed qualquer coisa`
-}
+};

5c20a55981e9cb15290560facd87457c8ef3f81e
zerinho6 <zerinho6@gmail.com>
2018-07-05 17:29:20 -0300

    Update say.js

--- comandos/say.js
+++ comandos/say.js
@@ -5,10 +5,10 @@ module.exports = {
 			try {
 				message.channel.send( args[ 0 ] );
 			} catch ( e ) {
-				var k = require( "../comandos/avatar.js" );
-				k.special( message , message.member , e );
-		    }
-	    }
+				var helper = require( "../helper.js" );
+				helper.error_message( message , message.member , e );
+			}
+		}
 	},
 	description: "Faça o bot enviar a mensagem que você quiser.",
 	photo: "https://i.imgur.com/zC4IxAZ.png",

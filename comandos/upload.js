7d3efef7a775d7b0b0b5a996bca38504d5db80bc
zerinho6 <zerinho6@gmail.com>
2018-07-05 17:34:03 -0300

    Update upload.js

--- comandos/upload.js
+++ comandos/upload.js
@@ -1,29 +1,30 @@
 var config = require( "../config.json" );
 module.exports = {
-	run: ( bot, message , args ) => {
-		if( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
+	run: ( bot , message , args ) => {
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
+		
 		var formatos = [ "png" , "jpg" , "gif" ],
-		k = require( "../comandos/avatar.js" ),
+		helper = require( "../helper.js" ),
 		to_upload = message.attachments,
 		user = message.author,
 		Discord = require( "discord.js" ),
 		embed = new Discord.RichEmbed(),
 		imgur = require( "imgur" );
+		
 		if ( to_upload.size >= 1 ) {
 			if ( formatos.find( s => to_upload.first().url.endsWith( s ) ) ) {
-				imgur.uploadUrl( to_upload.first().url ).then(function ( json ) {
-					
-				   embed.setAuthor( user.username , user.displayAvatarURL );
-				    embed.setColor( message.member.displayHexColor );
-				    embed.setImage( json.data.link );
-				    embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
-				    embed.setThumbnail( "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Imgur_logo.svg/2000px-Imgur_logo.svg.png");
-				    embed.setTitle( ":frame_photo: | A imagem foi enviada com sucesso" );
-				    embed.addField( ":link: Link", json.data.link );
-				    message.channel.send( embed );
-				}).catch(function ( e ) {
-					k.special( message , message.member , e );
-				})
+				imgur.uploadUrl( to_upload.first().url ).then( function ( json ) {
+					embed.setAuthor( user.username , user.displayAvatarURL );
+					embed.setColor( message.member.displayHexColor );
+					embed.setImage( json.data.link );
+					embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
+					embed.setThumbnail( "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Imgur_logo.svg/2000px-Imgur_logo.svg.png");
+					embed.setTitle( ":frame_photo: | A imagem foi enviada com sucesso" );
+					embed.addField( ":link: Link", json.data.link );
+					message.channel.send( embed );
+				}).catch( function ( e ) {
+					helper.error_message( message , message.member , e );
+				});
 			} else {
 				message.reply( "Apenas são aceitos os formatos: " + formatos.join( " | " ) );
 			}

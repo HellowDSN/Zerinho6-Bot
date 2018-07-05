95d10170435ff5f343b9f41fb4b9a8d56c8e2d07
zerinho6 <zerinho6@gmail.com>
2018-07-05 17:30:15 -0300

    Update serverinfo.js

--- comandos/serverinfo.js
+++ comandos/serverinfo.js
@@ -1,11 +1,14 @@
 var config = require( "../config.json" );
 module.exports = {
-    run: ( bot, message , args ) => {
-		if( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
+    run: ( bot , message , args ) => {
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
+		
 		var moment = require( "moment" ),
 		Discord = require( "discord.js" ),
-		k = require( "../comandos/avatar.js" ),
-		embed = new Discord.RichEmbed();
+		helper = require( "../helper.js" ),
+		embed = new Discord.RichEmbed(),
+		FieldDescriptions = [ "<a:cursor:404001393360502805> | Nome do servidor" , ":busts_in_silhouette: | Membros" , ":crown: | Dono" , "<:discord:314003252830011395> | ID" , ":earth_americas: | Região do servidor" , ":calendar_spiral: | Criado em" , ":clipboard: | Quantidade de cargos" ],
+		FieldComplements = [ message.guild.name , message.guild.memberCount , message.guild.owner.user.tag , message.guild.id , message.guild.region , moment( message.guild.createdAt ) , message.guild.roles.size ];
 		moment.locale( "pt-BR" );
 		
 		if ( message.guild.iconURL ) {
@@ -16,23 +19,19 @@ module.exports = {
 		}
 		embed.setTimestamp();
 		embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
-		embed.addField( "<a:cursor:404001393360502805> | Nome do servidor" , message.guild.name , true );
 		embed.setColor( message.member.displayHexColor );
 		message.guild.fetchMembers();
-		embed.addField( ":busts_in_silhouette: | Membros" , message.guild.memberCount , true );
-		embed.addField( ":crown: | Dono" , message.guild.owner.user.tag , true );
-                embed.addField( "<:discord:314003252830011395> | ID" , message.guild.id , true );
-                embed.addField( ":earth_americas: | Região do servidor" , message.guild.region , true );
-                embed.addField( ":calendar_spiral: | Criado em" , moment( message.guild.createdAt ) , true );
-		embed.addField( ":clipboard: | Quantidade de cargos" , message.guild.roles.size , true );
+		for ( i = 0 ; i < FieldDescriptions.length ; i++ ) {
+			embed.addField( FieldDescriptions[ i ] , FieldComplements[ i ] , true );
+		}
 		try {
 			message.channel.send( embed );
 		} catch ( e ) {
-			k.special( message , message.member , e );
+			helper.error_message( message , message.member , e );
 		}
-    },
-    description: "Mostra algumas informações sobre o servidor.",
-    photo: "https://i.imgur.com/4pQAEHl.png",
-    permission: "Nenhuma informação necessaria",
-    use: ` ${ config.prefixes[ 0 ] }serverinfo`
+	},
+	description: "Mostra algumas informações sobre o servidor.",
+	photo: "https://i.imgur.com/4pQAEHl.png",
+	permission: "Nenhuma informação necessaria",
+	use: `${ config.prefixes[ 0 ] }serverinfo`
 };

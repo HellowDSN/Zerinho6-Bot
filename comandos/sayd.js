65395a389040d5a53db5b3e6831b923dd8fdfd1a
zerinho6 <zerinho6@gmail.com>
2018-07-05 17:29:42 -0300

    Update sayd.js

--- comandos/sayd.js
+++ comandos/sayd.js
@@ -1,14 +1,15 @@
 var config = require( "../config.json" );
 module.exports = {
-	run: ( bot, message , args ) => {
-		if( !message.channel.permissionsFor( bot.user.id ).has( "MANAGE_MESSAGES" ) ) return message.reply( "Eu preciso da permissão de gerenciar mensagens para executar esse comando." );
+	run: ( bot , message , args ) => {
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "MANAGE_MESSAGES" ) ) return message.reply( "Eu preciso da permissão de gerenciar mensagens para executar esse comando." );
+		
 		if ( args[ 0 ] ) {
 			try {
 				message.delete();
-			        message.channel.send( args[ 0 ] );
+				message.channel.send( args[ 0 ] );
 			} catch ( e ) {
-				var k = require( "../comandos/avatar.js" );
-				k.special( message , message.member , e );
+				var helper = require( "../helper.js" );
+				helper.error_message( message , message.member , e );
 			}
 		}
 	},

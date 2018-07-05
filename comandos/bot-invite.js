7ffe487aeb7dc03e3494d9c9aacc16b2c7704815
zerinho6 <zerinho6@gmail.com>
2018-07-05 17:17:06 -0300

    Update bot-invite.js

--- comandos/bot-invite.js
+++ comandos/bot-invite.js
@@ -1,25 +1,21 @@
 var config = require( "../config.json" );
 module.exports = {
-	run: ( bot, message , args ) => {
-		if( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
+	run: ( bot , message , args ) => {
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
+		
 		var Discord = require( "discord.js" ),
 		embed = new Discord.RichEmbed(),
-		k = require( "../comandos/avatar.js" ),
 		user = message.author;
 		
 		if ( args[ 0 ] ) {
-			if ( !isNaN( parseInt( args[ 0 ] ) ) ) { 
-			    if ( args[ 0 ].length === 18 ) {
-				    embed.setAuthor( user.username , user.displayAvatarURL );
-		                    embed.setTimestamp();
-		                    embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
-		                    embed.setColor( message.member.displayHexColor );
-		                    embed.addField( ":link: | Link" , "https://discordapp.com/oauth2/authorize?&client_id=" + args[ 0 ] + "&scope=bot" );
-				        try {
-						message.channel.send( embed );
-					} catch ( e ) {
-						k.special( message , message.member , e );
-					}
+			if ( !isNaN( parseInt( args[ 0 ] ) ) ) {
+				if ( args[ 0 ].length === 18 ) {
+					embed.setAuthor( user.username , user.displayAvatarURL );
+					embed.setTimestamp();
+					embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
+					embed.setColor( message.member.displayHexColor );
+					embed.addField( ":link: | Link" , "https://discordapp.com/oauth2/authorize?&client_id=" + args[ 0 ] + "&scope=bot" );
+					message.channel.send( embed );
 				} else {
 					message.reply( "Um ID tem 18 caracteres, eu sei diferenciar" );
 				}

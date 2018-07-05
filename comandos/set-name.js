f805289f3f36a4fee2b29b368e9e53bfb987b770
zerinho6 <zerinho6@gmail.com>
2018-07-05 17:31:55 -0300

    Update set-name.js

--- comandos/set-name.js
+++ comandos/set-name.js
@@ -1,18 +1,19 @@
 var config = require( "../config.json" );
 module.exports = {
-	run: ( bot, message , args ) => {
+	run: ( bot , message , args ) => {
 		if ( !message.member.hasPermission( "MANAGE_GUILD" ) ) return message.reply( "Você precisa da permissão de gerenciar servidor para executar esse comando." );
-                if ( !message.guild.member( bot.user.id ).hasPermission( "MANAGE_GUILD" ) ) return message.reply( "Eu preciso da permissão de gerenciar servidor para executar esse comando." );
-		var k = require( "../comandos/avatar.js" ),
+		if ( !message.guild.member( bot.user.id ).hasPermission( "MANAGE_GUILD" ) ) return message.reply( "Eu preciso da permissão de gerenciar servidor para executar esse comando." );
+		
+		var helper = require( "../helper.js" ),
 		nome = message.content.split( " " ).slice( 1 ).join( " " );
 		
 		if ( nome ) {
 			if ( nome.length < 100 ||  nome.length > 2 ) {
 				try {
 					message.guild.setName( nome );
-					message.channel.send( "O nome do servidor agora é: **" + nome + "**." );
+					message.channel.send( "O nome do servidor agora é: " + helper.bold( nome ) + "." );
 				} catch ( e ) {
-					k.special( message , message.member , e );
+					helper.error_message( message , message.member , e );
 				}
 			} else {
 				message.reply( "O minimo de caracteres permitido é **2** e o maximo é **100**.");

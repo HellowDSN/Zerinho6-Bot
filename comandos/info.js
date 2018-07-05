7d405e4db723ae7ad576915b324666a415b8cdde
zerinho6 <zerinho6@gmail.com>
2018-07-05 17:22:32 -0300

    Update info.js

--- comandos/info.js
+++ comandos/info.js
@@ -1,43 +1,78 @@
 var config = require( "../config.json" );
 module.exports = {
-	run: ( bot, message , args ) => {
-		if( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
-		var k = require( "../comandos/avatar.js" ),
-		Discord = require( "discord.js" ),
-		embed = new Discord.RichEmbed(),
-		config = require( "../config.json" ),
-		startTime = Date.now(),
-		q = require( "../comandos/ping.js" ),
-		segundos = Math.floor( process.uptime() % 60 ),
-		minutos = Math.floor( process.uptime() / 60 % 60 ),
-		user = message.author,
-		hora = Math.floor( process.uptime() / 3600 % 24 );
+	run: ( bot , message , args ) => {
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
 		
-		embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
-		message.channel.send( embed ).then( message => {
-			var endTime = Date.now(),
-			ping = Math.round( endTime - startTime );
-			
-			embed.setAuthor( user.username , user.displayAvatarURL );
-			embed.setTimestamp();
-			embed.setColor( message.member.displayHexColor );
-			embed.setTitle( "<:botTag:230105988211015680> | " + config.bot_name );
-			embed.addField( "<:online:313956277808005120> | Uptime" , "```JavaScript\n" + hora + ":" + minutos + ":" + segundos + "```" , true );
-			embed.addField( "<:discord:314003252830011395> | Discord.js ver." , "```" + Discord.version + "```" , true );
-			embed.addField( ":wrench: | NPM ver." , "```" + process.version + "```" , true );
-			embed.addField( "<a:cursor:404001393360502805> | Servidores" , "```" + bot.guilds.size + "```" , true );
-			embed.addField( ":crown: | Bot Owner" , "```" + config.discord_usertag + "```" , true );
-			embed.addField( q.special( ping ) + " | Ping" , "```JavaScript\n" + ping + "ms```" , true );
-			embed.addField( ":gear: | RAM" , "```" + Math.round( process.memoryUsage().heapUsed / 1024 / 1024 ) + "MB```", true );
-			try {
-				message.edit( embed );
-			} catch ( e ) {
-				k.special( message , message.member , e );
+		var Discord = require( "discord.js" ),
+		fs = require( "fs" ),
+		user = message.author,
+		thing,
+		helper = require( "../helper.js" ),
+		files = fs.readdirSync( "./comandos/" ),
+		embed = new Discord.RichEmbed(),
+		frases = [ 
+		"Dia bom ou dia ruim, não deixa de ser um dia." , 
+		"Já botou uma tesoura na tomada? Eu já..não foi muito legal.",
+		"Já ouvi falar que um humano pode ficar 2 meses sem comer, não sei se é verdade.",
+		"E3 2018 foi uma merda.",
+		"Battlefield 5 não é Fortnite com grafico.",
+		"Gosta de jogos em bits? Procure por Celeste, é legal :).",
+		"Não copie o trabalho dos outros, aprenda com eles.",
+		"H",
+		"E",
+		"A vida não vai me fuder se eu fuder minha vida antes ~Feels#1039",
+		"AAAA A A A E O A I E U A O AAAA A AA A.",
+		"O trabalho árduo é capaz de polir as pedras mais asperas ~Kali#3099",
+		"Minha #$*# concentrada pode perfurar rochas ~'Rook#2832",
+		"Lei n° 3 da vida: Eu uma discussão contra uma mulher você NUNCA está certo, exceto se você for traficante. ~Naghlight#7512",
+		"Legal é meu p$# que tá do lado da minha bunda e não me come ~ Honux#6044",
+		"Eu gosto tanto da Cacau-Chan que se ela tivesse um pinto eu chupava ~Noogh#5672",
+		"ze.úserinfo ~Ellus#0666",
+		"ez.infouser ~Kage#3340",
+		"#JovemdaFaculdade ~Moru Zerinho6#6793",
+		"KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK ~Victin#5185",
+		"Com quantos neymars se faz um hexa? ~Feels#1039",
+		"Todo mundo pode te desapontar um dia, isso é fato...mas quem escolhe o quanto isso vai te afetar é você ~Moru Zerinho6#6793",
+		"O mundo é tão grande, mas enquanto você pensa sobre isso e não pensa sobre o como as coisas funcionam, você é excluido dele ~Moru Zerinho6#6793" ];
+		
+		try {
+			if ( !args[ 0 ] ) {
+				embed.setAuthor( " - " + user.username , user.displayAvatarURL );
+				embed.setColor( message.member.displayHexColor );
+				embed.setTimestamp();
+				embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
+				embed.setTitle( frases[ Math.floor( Math.random() * ( frases.length ) ) ] );
+				embed.addField( ":scroll: | Comandos" , helper.black_bg( files.join( ", " ) ).replace( /.js/gi, "" ) );
+				embed.addField( ":pen_ballpoint: | Prefix aceitas" , helper.black_bg( config.prefixes.join( "` `" ) ) );
+				if ( message.guild.iconURL ) {
+					embed.setThumbnail( message.guild.iconURL );
+				}
+				message.channel.send( embed );
+			} else if ( args[ 0 ] && files.find( s => args[ 0 ].toLowerCase().includes( s.replace( /.js/gi, "" ) ) ) ) {
+				thing = require( `../comandos/${ args[ 0 ].toLowerCase() }.js` );
+				embed.setAuthor( user.username, user.diaplayAvatarURL );
+				embed.setColor( message.member.displayHexColor );
+				embed.setTimestamp();
+				embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
+				embed.setImage( thing.photo );
+				embed.setDescription( "Troque o ``p`` que aparece nas imagens para a prefix..exemplo: ``ze.avatar``." );
+				embed.setTitle( frases[ Math.floor( Math.random() * ( frases.length - 1 ) ) ] );
+				embed.addField( ":book: | Descrição" , thing.description );
+				embed.addField( ":pencil: | Uso" , thing.use );
+				embed.addField( ":closed_lock_with_key: | Permissão necessaria", thing.permission ); 
+				if ( message.guild.iconURL ) {
+					embed.setThumbnail( message.guild.iconURL );
+				}
+				message.channel.send( embed );
+			} else {
+				message.reply( "Esse comando não existe.");
 			}
-		});
+		} catch ( e ) {
+			helper.error_message( message , message.member , e );
+		}
 	},
-	description: "Mostra algumas informações sobre o BOT",
-	photo: "Link da imagem",
-	permission: "Nenhuma permissão necessaria",
-	use: `${ config.prefixes[ 0 ] }info`
+	description: "Mostra o manual de ajuda do comando ou os comandos do bot.",
+	photo: "https://i.imgur.com/71NcAOS.png",
+	permission: "Nenhuma permissão necessaria.",
+	use: `${ config.prefixes[ 0 ] }help **ou** ${ config.prefixes[ 0 ] }help comando`
 };

2e748168f4696beb65734847bb26c950167519e0
zerinho6 <zerinho6@gmail.com>
2018-07-15 11:56:28 -0300

    bruh²

--- comandos/sayd.js
+++ comandos/sayd.js
@@ -1,16 +1,20 @@
 var config = require( "../config.json" );
 module.exports = {
 	run: ( bot , message , args ) => {
+		var argument = message.content.split( " " ).slice( 1 ).join( " " );
+		
 		if ( !message.channel.permissionsFor( bot.user.id ).has( "MANAGE_MESSAGES" ) ) return message.reply( "Eu preciso da permissão de gerenciar mensagens para executar esse comando." );
 		
-		if ( args[ 0 ] ) {
+		if ( argument ) {
 			try {
 				message.delete();
-				message.channel.send( args[ 0 ] );
+				message.channel.send( argument );
 			} catch ( e ) {
 				var helper = require( "../helper.js" );
 				helper.error_message( message , message.member , e );
 			}
+		} else {
+			message.reply( "O que diabo eu devo dizer?" );
 		}
 	},
 	description: "Faça o bot enviar a mensagem que você quiser, mas ele vai deletar a mensagem que você utilizou para executar o comando.",

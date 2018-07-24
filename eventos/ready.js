180b85a17e193d780405d6d1dcd20f17587f631e
zerinho6 <zerinho6@gmail.com>
2018-07-24 13:12:11 -0300

    Update ready.js

--- eventos/ready.js
+++ eventos/ready.js
@@ -1,6 +1,6 @@
 exports.run = function() {
-	var chalk = require( "chalk" ),
-	config = require( "../config.json" );
+	const chalk = require( "chalk" );
+	var config = require( "../config.json" );
 	
 	console.log( chalk.green( "Oi " ) + chalk.blue( config.name ) + "\nPessoal: " + chalk.blue( config.personal ) + "\nBot Name: " + chalk.blue( config.bot_name ) + "\nPrefixes " + chalk.blue( config.prefixes.join( " , " ) ) );
 };

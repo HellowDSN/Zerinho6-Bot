46f97fbb79304adf6d4581279b475693871d14d3
zerinho6 <zerinho6@gmail.com>
2018-08-08 17:46:41 -0300

    Update ready.js

--- eventos/ready.js
+++ eventos/ready.js
@@ -1,6 +1,12 @@
 exports.run = function() {
-	const chalk = require( "chalk" );
-	var config = require( "../config.json" );
+	const chalk = require( "chalk" ),
+	moment = require( "moment" ),
+	fs = require( "fs" ),
+	ini = require( "ini" );
+	var helper = require( "../helper.js" ),
+	config = require( "../config.json" ),
+	language = config.language,
+	language = ini.parse( fs.readFileSync( "./languages/" + language + ".ini" , "utf-8" ) );
 	
-	console.log( chalk.green( "Oi " ) + chalk.blue( config.name ) + "\nPessoal: " + chalk.blue( config.personal ) + "\nBot Name: " + chalk.blue( config.bot_name ) + "\nPrefixes " + chalk.blue( config.prefixes.join( " , " ) ) );
+	console.log( chalk.green( language.Hi ) + chalk.blue( config.name ) + "\n" + language.Personal  + chalk.blue( config.personal ) + language.BotName + chalk.blue( config.bot_name ) + "\nPrefixes " + chalk.blue( config.prefixes.join( " , " ) ) );
 };

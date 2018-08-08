3e54e5a05b7e7cc2d8f9b89ebbc91ef36c29291f
zerinho6 <zerinho6@gmail.com>
2018-08-08 17:49:21 -0300

    Update avatar.js

--- comandos/avatar.js
+++ comandos/avatar.js
@@ -1,57 +1,38 @@
 module.exports = {
-	run: ( bot , message , args , language ) => {
-		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( language.Bot_need_permission + language.embed_links + language.ToExecute );
+	run: ( bot, message , args , language ) => {
+		var mencionado = message.mentions.users.first(),
+		helper = require( "../helper.js" );
 
-		const Discord = require( "discord.js" );
-		var helper = require( "../helper.js" ),
-		user,
-		t_guild,
-		embed = new Discord.RichEmbed();
-
-		if ( args[ 0 ] && args[ 0 ] === language.avatar_guild ) {
-			try {
-				t_guild = message.guild;
-				if ( t_guild.iconURL ) {
-					embed.setAuthor( t_guild.name , t_guild.iconURL );
-					embed.setTitle( language.avatar_guild_icon + helper.bracket( t_guild.name , "(" ) );
-					embed.setImage( t_guild.iconURL );
-					embed.setDescription( language.avatar_guild_InLinkForm + t_guild.iconURL + ".png" );
-					embed.setTimestamp();
-					embed.setColor( message.member.displayHexColor );
-					embed.setFooter( language.CreatedBy );
-					message.channel.send( embed );
+		if ( message.guild.member( bot.user.id ).hasPermission( "BAN_MEMBERS" ) ) {
+			if ( message.member.hasPermission( "BAN_MEMBERS" ) ) {
+				if ( mencionado ) {
+					if ( message.guild.member( bot.user.id ).highestRole.position > message.guild.member( mencionado ).highestRole.position ) {
+						if ( message.member.highestRole.position > message.guild.member( mencionado ).highestRole.position ) {
+							if ( mencionado !== message.guild.owner ) {
+								//Jezz, Odeio usar essa quantidade de "if" ~Zerinho6
+								try {
+									message.guild.ban( mencionado );
+									message.reply( language.ban_user_banned );
+								} catch ( e ) {
+									helper.error_message( message , message.member , e );
+								}
+							} else {
+								message.reply( language.ban_user_isOwner );
+							}
+						} else {
+							message.reply( language.ban_user_HaveHighterRole );
+						}
+					} else {
+						message.reply( language.ban_user_HaveHighterRoleThanBot );
+					}
 				} else {
-					message.reply( language.avatar_guild_noIcon );
+					message.reply( language.ban_NoMention );
 				}
-			} catch ( e ) {
-				helper.error_message( message , message.member , e );
-			}
-		} else if ( message.mentions.users.first() ) {
-			try {
-				user = message.mentions.users.first();
-				embed.setAuthor( user.username , user.displayAvatarURL );
-				embed.setImage( user.displayAvatarURL );
-				embed.setTitle( language.avatar_avatarOf + user.tag );
-				embed.setTimestamp();
-				embed.setColor( message.member.displayHexColor );
-				embed.setFooter( language.CreatedBy );
-				message.channel.send( embed );
-			} catch ( e ) {
-				helper.error_message( message , message.member , e );
+			} else {
+				message.reply( language.ban_NoPermission );
 			}
 		} else {
-			try {
-				let user = message.author;
-				embed.setAuthor( user.username, user.displayAvatarURL );
-				embed.setColor( message.member.displayHexColor );
-				embed.setTimestamp();
-				embed.setImage( user.displayAvatarURL );
-				embed.setTitle( language.avatar_user_avatar );
-				embed.setFooter( language.CreatedBy );
-				message.channel.send( embed );
-			} catch ( e ) {
-				helper.error_message( message , message.member , e );
-			}
+			message.reply( language.ban_BotNoPermisson );
 		}
 	}
 };

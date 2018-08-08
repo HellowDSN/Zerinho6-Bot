ad07711d5ed24944b12f4e0635419438b27311f6
zerinho6 <zerinho6@gmail.com>
2018-08-08 18:00:31 -0300

    Update sayd.js

--- comandos/sayd.js
+++ comandos/sayd.js
@@ -1,24 +1,18 @@
-var config = require( "../config.json" );
 module.exports = {
-	run: ( bot , message , args ) => {
-		var argument = message.content.split( " " ).slice( 1 ).join( " " );
+	run: ( bot, message , args , language ) => {
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "MANAGE_MESSAGES" ) ) return message.reply( language.Bot_need_permission + language.manage_messages + language.ToExecute );
+	    
+		var helper = require( "../helper.js" );
 		
-		if ( !message.channel.permissionsFor( bot.user.id ).has( "MANAGE_MESSAGES" ) ) return message.reply( "Eu preciso da permissão de gerenciar mensagens para executar esse comando." );
-		
-		if ( argument ) {
+		if ( args[ 0 ] ) {
 			try {
 				message.delete();
-				message.channel.send( argument );
+				message.channel.send( args[ 0 ] );
 			} catch ( e ) {
-				var helper = require( "../helper.js" );
 				helper.error_message( message , message.member , e );
-			}
-		} else {
-			message.reply( "O que diabo eu devo dizer?" );
+		    }
+	    } else {
+			message.reply( language.NoArgument );
 		}
-	},
-	description: "Faça o bot enviar a mensagem que você quiser, mas ele vai deletar a mensagem que você utilizou para executar o comando.",
-	photo: "https://i.imgur.com/a6tQBXE.png",
-	permission: "Nenhuma permissão necessaria",
-	use: `${ config.prefixes[ 0 ] }say mensagem`
+	}
 };

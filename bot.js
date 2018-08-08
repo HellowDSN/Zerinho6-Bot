f587cedba056f2d6f2d3fb79410840157ff0f671
zerinho6 <zerinho6@gmail.com>
2018-08-08 17:41:47 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,15 @@
 "use strict";
 const Discord = require( "discord.js" ),
-fs = require( "fs" ),
 moment = require( "moment" ),
-chalk = require( "chalk" );
+chalk = require( "chalk" ),
+fs = require( "fs" ),
+ini = require( "ini" );
 var bot = new Discord.Client(),
-config = require( "./config.json" );
-moment().format();
-moment.locale( "pt-BR" );
-console.log( moment.locale() );
+config = require( "./config.json" ),
+language = config.language,
+language = ini.parse( fs.readFileSync( "./languages/" + language + ".ini" , "utf-8" ) );
+
+console.log( language.StartLanguage + " " + moment.locale() );
 
 fs.readdir( "./eventos/" , ( err, files ) => {
 
@@ -17,10 +19,10 @@ fs.readdir( "./eventos/" , ( err, files ) => {
 
     files.forEach( ( file ) => {
         try{
-          bot.on( file.replace( /.js/gi, "" ) , require( "./eventos/" + file ).run );
-          console.log( chalk.green( file + " Carregado." ) );
+          bot.on( file.replace( /.js/gi, "" ) , require( "./eventos/" + file).run );
+          console.log( chalk.green( file + language.Loaded ) );
         } catch( e ) {
-          console.error( chalk.red( "Erro ao carregar o evento " + file , e ) );
+          console.error( chalk.red( language.LoadEventError + file , e ) );
         }
     }); 
 });

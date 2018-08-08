4ec6fa12cf6bcbb3e30ceb9bcaf7c6edc79ce044
zerinho6 <zerinho6@gmail.com>
2018-08-08 18:03:38 -0300

    Update set-name.js

--- comandos/set-name.js
+++ comandos/set-name.js
@@ -1,29 +1,24 @@
-var config = require( "../config.json" );
 module.exports = {
-	run: ( bot , message , args ) => {
-		if ( !message.member.hasPermission( "MANAGE_GUILD" ) ) return message.reply( "Você precisa da permissão de gerenciar servidor para executar esse comando." );
-		if ( !message.guild.member( bot.user.id ).hasPermission( "MANAGE_GUILD" ) ) return message.reply( "Eu preciso da permissão de gerenciar servidor para executar esse comando." );
-		
+	run: ( bot , message , args , language ) => {
+		if ( !message.member.hasPermission( "MANAGE_GUILD" ) ) return message.reply( language.User_need_permission + language.manage_guild + language.ToExecute );
+		if ( !message.guild.member( bot.user.id ).hasPermission( "MANAGE_GUILD" ) ) return message.reply( language.Bot_need_permission + language.manage_guild + language.ToExecute );
+
 		var helper = require( "../helper.js" ),
 		nome = message.content.split( " " ).slice( 1 ).join( " " );
-		
+
 		if ( nome ) {
-			if ( nome.length < 100 ||  nome.length > 2 ) {
+			if ( nome.length < 100 && nome.length > 2 ) {
 				try {
 					message.guild.setName( nome );
-					message.channel.send( "O nome do servidor agora é: " + helper.bold( nome ) + "." );
+					message.channel.send( language.setName_NowTheGuildNameIs + helper.bold( nome ) );
 				} catch ( e ) {
 					helper.error_message( message , message.member , e );
 				}
 			} else {
-				message.reply( "O minimo de caracteres permitido é **2** e o maximo é **100**.");
+				message.reply( language.setName_CharactersLimits );
 			}
 		} else {
-			message.reply( "Você não escreveu qual vai ser o novo nome do servidor." );
+			message.reply( language.setName_NoArgs );
 		}
-	},
-	description: "Altera o nome do servidor",
-	photo: "https://i.imgur.com/qs6fatE.png",
-	permission: "MANAGE_GUILD",
-	use: `${ config.prefixes[ 0 ] }set-name blablabla`
+	}
 };

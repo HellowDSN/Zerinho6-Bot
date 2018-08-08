4a968840b585574599c937778ee00e020c90e8f6
zerinho6 <zerinho6@gmail.com>
2018-08-08 17:51:32 -0300

    Update embed.js

--- comandos/embed.js
+++ comandos/embed.js
@@ -1,30 +1,23 @@
-var config = require( "../config.json" );
 module.exports = {
-	run: ( bot , message , args ) => {
-		var argument = message.content.split( " " ).slice( 1 ).join( " " );
-		
-		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
-		
-		if ( argument ) {
+	run: ( bot , message , args , language ) => {
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( language.Bot_need_permission + language.embed_links + language.ToExecute );
+
+		if ( args[ 0 ] ) {
 			try {
-				const Discord = require( "discord.js" );
-				var user = message.author,
+				var Discord = require( "discord.js" ),
+				user = message.author,
 				embed = new Discord.RichEmbed();
-				
+
 				embed.setAuthor( user.username, user.displayAvatarURL );
-				embed.addField( "** **" , argument );
+				embed.addField( "** **" , args[ 0 ] );
 				embed.setColor( message.member.displayHexColor );
 				embed.setTimestamp();
-				embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
+				embed.setFooter( language.CreatedBy );
 				message.channel.send( embed );
 			} catch ( e ) {
 				var helper = require( "../helper.js" );
 				helper.error_message( message , message.member , e );
 			}
 		}
-	},
-	description: "Envia uma mensagem em embed",
-	photo: "https://i.imgur.com/QGRnvlp.png",
-	permission: "Nenhuma permissão necessaria.",
-	use: `${ config.prefixes[ 0 ] }embed qualquer coisa`
+	}
 };

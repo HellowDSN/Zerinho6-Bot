96f3937a41f21db882aa6754ede3656445a131b2
zerinho6 <zerinho6@gmail.com>
2018-08-08 17:53:42 -0300

    Update google.js

--- comandos/google.js
+++ comandos/google.js
@@ -1,39 +1,34 @@
-var config = require( "../config.json" );
 module.exports = {
-	run: ( bot , message , args ) => {
+	run: ( bot , message , args , language ) => {
 		var argument = message.content.split( " " ).slice( 1 ).join( " " );
 		const google = require( "google" );
-		
+
 		if ( argument ) {
-			
+
 			google( argument , function ( e , res ) {
-				
+
 				var i = 0,
 				helper = require( "../helper.js" );
-				
+
 				if ( e ) {
-					message.reply( "Ocorreu um erro.\n" + e );
+					message.reply( language.Error+ "\n" + e );
 				}
-				
+
 				while ( res.links[ i ] === null ) {
-					i++
+					i++; //Magic.
 				}
-		
+
 				if ( res.links[ i ] === undefined ) {
-					message.reply( "Sua pesquisa não teve resultados" );
+					message.reply( language.google_noResults );
 					return;
 				}
-				
-				message.channel.send( "Resultado para " + helper.bold( argument ) + "\n\n" + res.links[ i ].title + "\n" + res.links[ i ].href );
-				
+
+				message.channel.send( language.google_resultsFor + helper.bold( argument ) + "\n\n" + res.links[ i ].title + "\n" + res.links[ i ].href );
+
 			});
-			
+
 		} else {
-			message.reply( "Você não botou o que eu devo pesquisar :/" );
+			message.reply( language.google_NoArgs );
 		}
-	},
-	description: "Faz um pesquisa no google.",
-	photo: "https://i.imgur.com/hccuBVv.png",
-	permission: "Nenhuma permissão necessaria",
-	use: `${ config.prefixes[ 0 ] }google conteudo a pesquisar`
+	}
 };

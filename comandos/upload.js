47fcdf2922763152daeb4e38e75ab11cf67c2128
zerinho6 <zerinho6@gmail.com>
2018-08-08 18:05:15 -0300

    Update upload.js

--- comandos/upload.js
+++ comandos/upload.js
@@ -1,39 +1,30 @@
-var config = require( "../config.json" );
 module.exports = {
-	run: ( bot , message , args ) => {
-		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
+	run: ( bot , message , args , language ) => {
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( language.Bot_need_permission + language.embed_links + language.ToExecute );
 		
 		const Discord = require( "discord.js" ),
 		imgur = require( "imgur" );
-		var formatos = [ "png" , "jpg" , "gif" ],
-		helper = require( "../helper.js" ),
-		to_upload = message.attachments,
+		var helper = require( "../helper.js" ),
 		user = message.author,
 		embed = new Discord.RichEmbed();
 		
-		if ( to_upload.size >= 1 ) {
-			if ( formatos.find( s => to_upload.first().url.endsWith( s ) ) ) {
-				imgur.uploadUrl( to_upload.first().url ).then( function ( json ) {
-					embed.setAuthor( user.username , user.displayAvatarURL );
-					embed.setColor( message.member.displayHexColor );
-					embed.setImage( json.data.link );
-					embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
-					embed.setThumbnail( "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Imgur_logo.svg/2000px-Imgur_logo.svg.png");
-					embed.setTitle( ":frame_photo: | A imagem foi enviada com sucesso" );
-					embed.addField( ":link: Link", json.data.link );
-					message.channel.send( embed );
-				}).catch( function ( e ) {
-					helper.error_message( message , message.member , e );
-				});
-			} else {
-				message.reply( "Apenas são aceitos os formatos: " + formatos.join( " | " ) );
-			}
+		if ( helper.get_image( message , true ) ) {
+			var imagem = helper.get_image( message, false );
+			
+			imgur.uploadUrl( imagem ).then( function ( json ) {
+				embed.setAuthor( user.username , user.displayAvatarURL );
+				embed.setColor( message.member.displayHexColor );
+				embed.setImage( json.data.link );
+				embed.setFooter( language.CreatedBy );
+				embed.setThumbnail( language.upload_imgurLogo );
+				embed.setTitle( language.upload_imageUploaded );
+				embed.addField( language.upload_link , json.data.link );
+				message.channel.send( embed );
+			}).catch( function ( e ) {
+				helper.error_message( message , message.member , e );
+			});
 		} else {
-			message.reply( "Você tem que enviar uma imagem para eu fazer o upload." );
+			message.reply( language.upload_ImageNoDetected );
 		}
-	},
-	description: "Faz o upload da imagem enviada por você para o imgur.",
-	photo: "https://i.imgur.com/IQVfCTE.png",
-	permission: "Nenhuma permissão necessaria.",
-	use: `${ config.prefixes[ 0 ] }upload (envie uma imagem junto com o comando)`
+	}
 };

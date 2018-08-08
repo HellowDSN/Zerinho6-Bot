4134448ddab1bea4f6af3165716a76f0d987d69f
zerinho6 <zerinho6@gmail.com>
2018-08-08 17:58:18 -0300

    Update poll.js

--- comandos/poll.js
+++ comandos/poll.js
@@ -1,31 +1,26 @@
-var config = require( "../config.json" );
 module.exports = {
-	run: ( bot , message , args ) => {
-		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
-		
+	run: ( bot , message , args , language ) => {
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( language.Bot_need_permission + language.embed_links + language.ToExecute );
+
+		const Discord = require( "discord.js" );
 		var helper = require( "../helper.js" ),
-		Discord = require( "discord.js" ),
 		user = message.author,
-		SPELLCARD_DIVISION = message.content.split( " " ).slice( 1 ).join( " " ).split( " | " ), 
+		SPELLCARD_DIVISION = message.content.split( " " ).slice( 1 ).join( " " ).split( " | " ),
 		embed = new Discord.RichEmbed();
-		
+
 		if ( SPELLCARD_DIVISION[ 0 ] && SPELLCARD_DIVISION[ 1 ] ) { //Eu tenho uns problemas ~zerinho6
 			try {
 				embed.setAuthor( user.username , user.displayAvatarURL );
 				embed.setTimestamp();
-				embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
+				embed.setFooter( language.CreatedBy );
 				embed.setColor( message.member.displayHexColor );
 				embed.addField( SPELLCARD_DIVISION[ 0 ] , SPELLCARD_DIVISION[ 1 ] );
 				message.channel.send( embed ).then( message => {
-					message.react( "👍" ).then( message.react( "👎" ) );
+					message.react( language.poll_thumbsup ).then( message.react( language.poll_thumbsdown ) );
 				});
 			} catch ( e ) {
 				helper.error_message( message , message.member , e );
 			}
-		} 
-	},
-	description: "Cria uma votação feita de reações.",
-	photo: "https://i.imgur.com/B0M9ppp.png",
-	permission: "Nenhuma permissão necessaria.",
-	use: `${ config.prefixes[ 0 ] }poll Titulo | conteudo`
+		}
+	}
 };

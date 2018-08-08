980096de7600051b51ba5d7209b5c2560f22def7
zerinho6 <zerinho6@gmail.com>
2018-08-08 17:57:50 -0300

    Update ping.js

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,7 +1,6 @@
-var config = require( "../config.json" );
-module.exports = { 
-	run: ( bot , message , args ) => {
-		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
+module.exports = {
+	run: ( bot , message , args , language ) => {
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( language.Bot_need_permission + language.embed_links + language.ToExecute );
 		
 		const Discord = require( "discord.js" );
 		var old = new Date(),
@@ -9,34 +8,24 @@ module.exports = {
 		helper = require( "../helper.js" ),
 		now,
 		embed = new Discord.RichEmbed();
-		
+
 		function emoji( ping ) {
-			var array = [ "<:circulo_vermelho:428733167877226497>" , "<:circulo_laranja:428733119399460865>" , "<:circulo_amarelo:428733064605335573>" , "<:circulo_verde:428732952092999680>" ],
+			var array = [ language.ping_ReallyHighPing , language.ping_HighPing , language.ping_MediumPing , language.ping_LowPing ],
 			pings = [ 3000 , 1500 , 250 , 120 ];
-			
-			return array[ pings.findIndex( p => ping =< p ) ];
+
+			return array[ pings.findIndex( p => ping > p ) ];
 		}
 		
-		embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
+		embed.setFooter( language.CreatedBy );
 		message.channel.send( embed ).then( message => {
 			now = new Date();
-			
+
 			embed.setTimestamp();
 			embed.setColor( message.member.displayHexColor );
 			embed.setAuthor( user.username, user.displayAvatarURL );
-			embed.addField( emoji( Math.round( now - old ) ) + " | Pong", helper.embed( Math.round( now - old ) + "ms" , "JavaScript" ) );
-			embed.addField( ":gear: **| RAM**" , helper.embed( `${ Math.round( process.memoryUsage().heapUsed / 1024 / 1024 ) }MB\u200B` , "JavaScript" ) );
+			embed.addField( emoji( Math.round( now - old ) ) + language.ping_Pong, helper.embed( Math.round( now - old ) + language.ms , "JavaScript" ) );
+			embed.addField( language.ping_RAM , helper.embed( `${ Math.round( process.memoryUsage().heapUsed / 1024 / 1024 ) + language.MB}\u200B` , "JavaScript" ) );
 			message.edit( embed );
 		});
-	},
-	description: "Mostra o ping do bot.",
-	photo: "https://i.imgur.com/VuN493P.png",
-	permission: "Nenhuma permissão necessaria.",
-	use: `${ config.prefixes[ 0 ] }ping`,
-	special: function emoji( ping ) {
-		var array = [ "<:circulo_vermelho:428733167877226497>" , "<:circulo_laranja:428733119399460865>" , "<:circulo_amarelo:428733064605335573>" , "<:circulo_verde:428732952092999680>" ],
-		pings = [ 3000 , 1500 , 250 , 120 ];
-		
-		return array[ pings.findIndex( p => ping > p ) ];
 	}
 };

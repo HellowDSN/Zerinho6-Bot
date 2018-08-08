80cfe1b5906897b5e84b57a5359f2a512951ad61
zerinho6 <zerinho6@gmail.com>
2018-08-08 17:52:18 -0300

    Update eval.js

--- comandos/eval.js
+++ comandos/eval.js
@@ -1,32 +1,26 @@
-var config = require( "../config.json" );
 module.exports = {
-	run: ( bot , message , args ) => {
-		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
+	run: ( bot , message , args , language ) => {
+		var config = require( "../config.json" );
+		
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( language.Bot_need_permission + language.embed_links + language.ToExecute );
 		if ( message.author.id !== config.dono ) return;
 		
 		const Discord = require( "discord.js" );
 		var helper = require( "../helper.js" ),
 		code = message.content.split( " " ).slice( 1 ).join( " " ),
-		FieldDescriptions = [ "Codigo" , "typeof" , "Resultado" ],
-		FieldComplements = [ helper.embed( code , "JavaScript" ) , helper.embed( typeof( code ) , "JavaArray" ) , helper.embed( eval( code ) , "JavaScript" ) ],
+		FieldDescriptions = [ language.Code , language.Typeof , language.Result ],
+		FieldComplements = [ helper.embed( code , "JavaScript" ) , helper.embed( typeof( code ) , "JavaScript" ) , helper.embed( eval( code ) , "JavaScript" ) ],
 		embed = new Discord.RichEmbed();
-		
+
 		try {
 			embed.setColor( message.member.displayHexColor );
-			embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
+			embed.setFooter( language.CreatedBy );
 			for ( let i = 0 ; i < FieldComplements.length ; i++ ) {
 				embed.addField( FieldDescriptions[ i ] , FieldComplements[ i ] , true );
 			}
 			message.channel.send( embed );
 		} catch ( e ) {
-			embed.setColor( message.member.displayHexColor );
-			embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
-			embed.addField( "Erro", helper.embed( e , "JavaScript" ) );
-			message.channel.send( embed );
+			helper.error_message( message , message.member , e );
 		}
-	},
-	description: "Executa um script.",
-	photo: "https://i.imgur.com/pUNLB9y.png",
-	permission: "Bot Owner",
-	use: `${ config.prefixes[ 0 ] }eval codigo`
+	}
 };

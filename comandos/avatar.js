740b430ef22d41424eb71ae7a88fe73d1b41e1cf
zerinho6 <zerinho6@gmail.com>
2018-08-08 17:48:51 -0300

    Update avatar.js

--- comandos/avatar.js
+++ comandos/avatar.js
@@ -1,27 +1,28 @@
-var config = require( "../config.json" );
 module.exports = {
-	run: ( bot , message , args ) => {
-		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( "Eu preciso da permissão de embed_links para executar esse comando." );
-		
+	run: ( bot , message , args , language ) => {
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "EMBED_LINKS" ) ) return message.reply( language.Bot_need_permission + language.embed_links + language.ToExecute );
+
 		const Discord = require( "discord.js" );
 		var helper = require( "../helper.js" ),
 		user,
 		t_guild,
 		embed = new Discord.RichEmbed();
-		
-		if ( args[ 0 ] && args[ 0 ] === "guild" ) {
+
+		if ( args[ 0 ] && args[ 0 ] === language.avatar_guild ) {
 			try {
 				t_guild = message.guild;
 				if ( t_guild.iconURL ) {
 					embed.setAuthor( t_guild.name , t_guild.iconURL );
-					embed.setTitle( "Icone do servidor" + helper.bracket( t_guild.name , "(" ) );
+					embed.setTitle( language.avatar_guild_icon + helper.bracket( t_guild.name , "(" ) );
 					embed.setImage( t_guild.iconURL );
-					embed.setDescription( "\nEm forma de link: " + t_guild.iconURL + ".png" );
+					embed.setDescription( language.avatar_guild_InLinkForm + t_guild.iconURL + ".png" );
 					embed.setTimestamp();
 					embed.setColor( message.member.displayHexColor );
-					embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
+					embed.setFooter( language.CreatedBy );
 					message.channel.send( embed );
-				} else message.reply( "Este servidor não tem um icone definido." );
+				} else {
+					message.reply( language.avatar_guild_noIcon );
+				}
 			} catch ( e ) {
 				helper.error_message( message , message.member , e );
 			}
@@ -30,10 +31,10 @@ module.exports = {
 				user = message.mentions.users.first();
 				embed.setAuthor( user.username , user.displayAvatarURL );
 				embed.setImage( user.displayAvatarURL );
-				embed.setTitle( "Avatar de " + user.tag );
+				embed.setTitle( language.avatar_avatarOf + user.tag );
 				embed.setTimestamp();
 				embed.setColor( message.member.displayHexColor );
-				embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
+				embed.setFooter( language.CreatedBy );
 				message.channel.send( embed );
 			} catch ( e ) {
 				helper.error_message( message , message.member , e );
@@ -45,16 +46,12 @@ module.exports = {
 				embed.setColor( message.member.displayHexColor );
 				embed.setTimestamp();
 				embed.setImage( user.displayAvatarURL );
-				embed.setTitle( "Seu avatar." );
-				embed.setFooter( "Zerinho6 Bot™ criado por Moru Zerinho6#6793" );
+				embed.setTitle( language.avatar_user_avatar );
+				embed.setFooter( language.CreatedBy );
 				message.channel.send( embed );
 			} catch ( e ) {
 				helper.error_message( message , message.member , e );
 			}
 		}
-	},
-	description: "Mostra o link do avatar/icon do(a) usuario/servidor",
-	photo: "https://i.imgur.com/ryTGep9.png",
-	permission: "Nenhuma permissão necessaria.",
-	use: `${ config.prefixes[ 0 ] }avatar **ou** ${ config.prefixes[ 0 ] }avatar @usuario **ou** ${ config.prefixes[ 0 ] }avatar server`,
+	}
 };

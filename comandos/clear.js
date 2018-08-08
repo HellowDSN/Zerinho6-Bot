718eb380e954d8e5c79f1fa451637e91d402e474
zerinho6 <zerinho6@gmail.com>
2018-08-08 17:50:32 -0300

    Update clear.js

--- comandos/clear.js
+++ comandos/clear.js
@@ -1,9 +1,8 @@
-var config = require( "../config.json" );
 module.exports = {
-	run: ( bot , message , args ) => {
-		if ( !message.member.hasPermission( "MANAGE_MESSAGES" ) ) return message.reply( "Você precisa da permissão de gerenciar mensagens para executar esse comando." );
-		if ( !message.channel.permissionsFor( bot.user.id ).has( "MANAGE_MESSAGES" ) ) return message.reply( "Eu preciso da permissão de gerenciar mensagens para executar esse comando." );
-		
+	run: ( bot , message , args , language ) => {
+		if ( !message.member.hasPermission( "MANAGE_MESSAGES" ) ) return message.reply( language.User_need_permission + language.clear_permission + language.ToExecute );
+		if ( !message.channel.permissionsFor( bot.user.id ).has( "MANAGE_MESSAGES" ) ) return message.reply( language.Bot_need_permission + language.clear_permission + language.ToExecute );
+
 		if ( args[ 0 ] ) {
 			if ( !isNaN( parseInt( args[ 0 ] ) ) ) {
 				var toDelete = parseInt( args[ 0 ] ),
@@ -11,22 +10,18 @@ module.exports = {
 				if ( toDelete < 100 ) {
 					try {
 						message.channel.bulkDelete( toDelete + 1 , true );
-						toDelete === 1 ? message.channel.send( toDelete + " mensagem foi deletada." ) : message.channel.send( toDelete + " mensagens foram deletadas." ); 
+						toDelete === 1 ? message.channel.send( toDelete + language.clear_message_deleted ) : message.channel.send( toDelete + language.clear_messages_deleted );
 					} catch ( e ) {
 						helper.error_message( message , message.member , e );
 					}
 				} else {
-					message.reply( "A quantidade no qual eu devo deletar deve ser menos de 100." );
+					message.reply( language.clear_reachedLimit );
 				}
 			} else {
-				message.reply( "A quantidade de mensagens que eu devo deletar deve ser em numeral." );
+				message.reply( language.clear_MustBeNumeral );
 			}
 		} else {
-			message.reply( "Apagar quantas mensagens? Se não sabe usar o comando, use o comando de ajuda." );
-		}  
-	},
-	description: "Apaga X mensagem(ns).",
-	photo: "https://i.imgur.com/At2fPJE.png",
-	permission: "MANAGE_MESSAGES",
-	use: `${ config.prefixes[ 0 ] }clear numero_de_mensagens_para_deletar`
+			message.reply( language.clear_NoArgs );
+		}
+	}
 };
